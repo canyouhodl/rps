33a5da7e621e80538cac460cc5de1250b5322a94
Damien <canyouhodl@gmail.com>
2021-08-04 11:21:51 -0700

    finished UI

--- script.js
+++ script.js
@@ -1,141 +1,225 @@
+let pScore = 0;
+let cScore = 0;
+
+const rockBtn = document.querySelector('.rockBtn');
+const paperBtn = document.querySelector('.paperBtn');
+const scissorsBtn = document.querySelector('.scissorsBtn');
+
+const compRock = document.querySelector('.compRock');
+const compPaper = document.querySelector('.compPaper');
+const compScissors = document.querySelector('.compScissors');
 
 
+const playerScore = document.querySelector('.playerScore');
+const compScore = document.querySelector('.compScore');
+
+const buttons = document.querySelectorAll('button')
+
+function disableButtons() {
+    buttons.forEach(btn => {
+        btn.disabled = true;
+    });
+};
+
+function main() {
+
+    rockBtn.addEventListener('click', () => {
+        playRound('rock');
+    })
+    
+    paperBtn.addEventListener('click', () => {
+        playRound('paper');
+    })
+    
+    scissorsBtn.addEventListener('click', () => {
+        playRound('scissors');
+    })
+
+
+}
 
 
 function computerPlay() {
-    let hand;
-    let num = Math.floor(Math.random()*3);
+    const rng = Math.floor(Math.random() * 3) + 1;
+    switch(rng) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+    }
+}
 
-    if (num === 0) {
-        hand = 'rock';
-    }
-    else if (num === 1) {
-        hand = 'paper';
-    }
-    else if (num === 2) {
-        hand = 'scissors';
-    }
-    else {
-        hand = 'invalid';
-    }
-    return hand;
+function show() {
+    compRock.classList.remove('hide');
+    compScissors.classList.remove('hide');
+    compPaper.classList.remove('hide');
+    rockBtn.classList.remove('hide');
+    paperBtn.classList.remove('hide');
+    scissorsBtn.classList.remove('hide');
 }
 
 
 
 
+function playRound(playerHand) {
+    const message = document.querySelector('.message');
+    let computerHand = computerPlay();
+    let result = '';
 
-
-function playRound() {
-    let playerSelection = prompt("Choose your hand").toLowerCase();
-    let computerSelection = computerPlay();
-
-
-    if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('You lose! Paper beats rock');
-        return 'lose';
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        console.log('Draw!')
-        return 'draw';
-    }
-    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log('You win! Scissors beat paper.')
-        return 'win';
-    }
-
-    else if (playerSelection === 'rock' && computerSelection === 'rock') {
-        console.log('Draw!');
-        return 'draw';
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'rock') {
-
-        console.log('You win! Paper beats rock.');
-        return 'win';
-    }
-    else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log('You lose! Rock beats scissors.');
-        return 'lose';
-    }
-
-    else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('You lose! Paper beats rock.');
-        return 'lose';
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        console.log('Draw!');
-        return 'draw';
-
-    }
-    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-
-        console.log('You Win! Scissors beat paper.')
-        return 'win';
-    }
-
-    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log('You Win! Rock beats scissors.')
-        return 'win';
-
-    }
-    else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('You lose! Scissors beat paper');
-        return 'lose';
-
-    }
-    else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
-        console.log('Draw!');
-        return 'draw';
-    }
-    else {
-        console.log('Invalid! Choose from rock, paper, and scissors')
-        return 'invalid';
-    }
-}
-
-
-function game() {
-    let humScore = 0;
-    let compScore = 0;
-    let gamesPlayed = 0;
-
-    while (gamesPlayed < 5) {
-        
-        let round = playRound();
-        switch(round) {
-            case 'win':
-                ++gamesPlayed;
-                humScore++;
-                console.log(`Score You: ${humScore} Computer: ${compScore}`);
-                break;
-            
-            case 'lose':
-                ++gamesPlayed;
-                compScore++;
-                console.log(`Score You: ${humScore} Computer: ${compScore}`);
-                break;
-            
-            case 'draw':
-                ++gamesPlayed;
-                humScore++;
-                compScore++;
-                console.log(`Score You: ${humScore} Computer: ${compScore}`);
-      
+    if(playerHand === 'rock' && computerHand === 'scissors' || 
+    playerHand === 'paper' && computerHand === 'rock' ||
+    playerHand === 'scissors' && computerHand === 'paper') {
+        if(computerHand === 'scissors') {
+            compRock.classList.add('hide');
+            compPaper.classList.add('hide');
         }
-    }
+        if (computerHand === 'rock') {
+            compScissors.classList.add('hide');
+            compPaper.classList.add('hide');
+        }
+        if (computerHand === 'paper') {
+            compRock.classList.add('hide');
+            compScissors.classList.add('hide');
+        }
+        if(playerHand === 'rock') {
+            scissorsBtn.classList.add('hide');
+            paperBtn.classList.add('hide');
+        }
+        if (playerHand === 'paper') {
+            rockBtn.classList.add('hide');
+            scissorsBtn.classList.add('hide');
+        }
+        if (playerHand === 'scissors') {
+            rockBtn.classList.add('hide');
+            paperBtn.classList.add('hide');
+        }
+        pScore++;
+        setTimeout(function() { show(); }, 1000);
+        playerScore.innerText = `Player: ${pScore}`;
+        compScore.innerText = `Computer: ${cScore}`;
 
-    if (humScore > compScore) {
-        console.log(`YOU WIN ${humScore} to ${compScore}!`);
-    }
-    else if (compScore > humScore) {
-        console.log(`YOU LOSE ${humScore} to ${compScore} :( `);
-    }
-    else {
-        console.log(`DRAW! ${humScore} to ${compScore}`)
-    }
+        result = `You win! ${playerHand} beats ${computerHand}`;
+        message.innerText = `You win! ${playerHand} beats ${computerHand}`;
 
+        if (pScore === 5) {
+            alert('You won the game! Reload the page to play again');
+            disableButtons();
+        }
+    } 
+    if(computerHand === 'rock' && playerHand === 'scissors' || 
+    computerHand === 'paper' && playerHand === 'rock' ||
+    computerHand === 'scissors' && playerHand === 'paper') {
+        if(computerHand === 'scissors') {
+            compRock.classList.add('hide');
+            compPaper.classList.add('hide');
+        }
+        if (computerHand === 'rock') {
+            compScissors.classList.add('hide');
+            compPaper.classList.add('hide');
+        }
+        if (computerHand === 'paper') {
+            compRock.classList.add('hide');
+            compScissors.classList.add('hide');
+        }
+        if(playerHand === 'rock') {
+            scissorsBtn.classList.add('hide');
+            paperBtn.classList.add('hide');
+        }
+        if (playerHand === 'paper') {
+            rockBtn.classList.add('hide');
+            scissorsBtn.classList.add('hide');
+        }
+        if (playerHand === 'scissors') {
+            rockBtn.classList.add('hide');
+            paperBtn.classList.add('hide');
+        }
+        cScore++;
+        setTimeout(function() { show(); }, 1000);
+        playerScore.innerText = `Player: ${pScore}`;
+        compScore.innerText = `Computer: ${cScore}`;
 
+        result = `Lose! ${computerHand} beats ${playerHand}`;
+        message.innterText = result;
+        
+        if (cScore === 5) {
+            buttons.disabled = true;
+            alert('You lost the game! Reload the page to play again');
+            disableButtons();
+        }
+    } 
+    if (playerHand === computerHand) {
+        if(computerHand === 'scissors') {
+            compRock.classList.add('hide');
+            compPaper.classList.add('hide');
+        }
+        if (computerHand === 'rock') {
+            compScissors.classList.add('hide');
+            compPaper.classList.add('hide');
+        }
+        if (computerHand === 'paper') {
+            compRock.classList.add('hide');
+            compScissors.classList.add('hide');
+        }
+        if(playerHand === 'rock') {
+            scissorsBtn.classList.add('hide');
+            paperBtn.classList.add('hide');
+        }
+        if (playerHand === 'paper') {
+            rockBtn.classList.add('hide');
+            scissorsBtn.classList.add('hide');
+        }
+        if (playerHand === 'scissors') {
+            rockBtn.classList.add('hide');
+            paperBtn.classList.add('hide');
+        }
+
+        setTimeout(function() { show(); }, 1000);
+        playerScore.innerText = `Player: ${pScore}`;
+        compScore.innerText = `Computer: ${cScore}`;
+
+        message.innerText = 'Draw!'
+    }
+    return
 
 }
 
-game();
+
+
+
+// function game(playerHand) {
+//     let pScore = 0;
+//     let cScore = 0;
+//     let compHand = computerPlay();
+//     while (pScore < 5 && cScore < 5) {
+
+//         const winner = playRound(playerHand);
+//         switch(winner) {
+//             case 'player':
+//                 ++pScore;
+//                 console.log(`You win. Score is ${pScore} to ${cScore}`)
+//                 break;
+//             case 'computer':
+//                 ++cScore;
+//                 console.log(`You Lose. Score is ${pScore} to ${cScore}`)
+//                 break;
+//             case 'draw':
+//                 console.log(`Draw. Score is ${pScore} to ${cScore}`)
+//                 break;
+//         }
+//     }
+
+//     if (pScore === 5) {
+//         console.log(`You won the game! Final score: ${pScore} to ${cScore}`);
+//         return 'player';
+//     }
+//     if (cScore === 5) {
+//         console.log(`You lost the game! Final score: ${pScore} to ${cScore}`);
+//         return 'computer';
+//     }
+// }
+
+
+
+main();
